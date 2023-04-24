46836b5892c868cc8a8961c11a38319c490b0756
Segev Finer <segev@swimm.io>
2023-04-24 20:36:56 +0300

    Initial Commit

---
+++ src/components/__tests__/HelloWorld.cy.ts
@@ -0,0 +1,18 @@
+import HelloWorld from '../HelloWorld.vue'
+
+describe('HelloWorld', () => {
+  it('playground', () => {
+    cy.mount(HelloWorld, { props: { msg: 'Hello Cypress' } })
+  })
+
+  it('renders properly', () => {
+    cy.mount(HelloWorld, { props: { msg: 'Hello Cypress' } })
+    cy.get('h1').should('contain', 'Hello Cypress')
+  })
+
+  it('exposes hello', () => {
+    cy.mount(HelloWorld, { props: { msg: 'Hello Cypress' } }).then(({ component }) => {
+      expect(component.hello).to.equal('World');
+    })
+  })
+})
